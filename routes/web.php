d95e86b5c0863d103cb5ae402ff004adcf7ea922
ThiagoMarceloSauer <coisadenoob27@gmail.com>
2025-08-08 20:08:48 -0300

    Commit inicial na nova branch dev

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Home';
+})->name('index');
+
+route::get('/produtos', function(){
+    return 'Produtos';
+});
+
+route::get(
+    '/produtos/{produto_id}/{nome}/{categoria}',
+    function(
+        int $produto_id,
+        string $nome,
+        string $categoria)
+        { echo ' Id_produto: ' . $produto_id. ' Nome: ' . $nome. ' Categoria: ' . $categoria; 
+    })->where('produto_id','[0+9]+');
+
+route::get('/cadastro', function(){
+    echo 'cadastrar';
+});
+
+route::get('/login', function(){
+    echo 'login';
+});
+
+route::get('/sobre', function(){
+    
+    echo 'sobre';
+})->name('sobre');
+
+route::get('/contato', function(){});
+
+route::prefix('admin')->group(function(){
+    route::get('/login',function(){
+        echo 'login adm';
+    });
+    route::get('/dashbord', function(){
+        echo 'painel principal';
+    })->name('admin.dashbord');
+    route::get('/clientes', function(
+        int $id_cliente){
+        echo 'lista de clientes';
+    })->where('id_cliente','[0+9]+');
+    route::get('/fornecedores',function(){
+        echo 'lista de fornecedores';
+    });
+    route::get('/produtos/{slug}',function(){
+        echo 'lista de produtos cadastrados';
+    })->where('slug','[A-Za-z0-9\-]+');
+
+});
